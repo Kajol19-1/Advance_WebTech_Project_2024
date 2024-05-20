04c4b7e0c5ff359434b72f7e9a48d756d23db9e7
kajol <kajol.99bd@gmail.com>
2024-05-20 19:39:40 +0600

    Migration part completed

---
+++ FirstTest/database/migrations/2024_05_20_132249_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id',255);
+            $table->string('name',255);
+            $table->string('s_id',255);
+            $table->string('email',255);
+            $table->date('dob',255);
+            $table->string('phone',255);
+
+            $table->timestamps(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
